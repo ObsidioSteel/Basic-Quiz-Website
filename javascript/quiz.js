7815f068350f9415a0beef5f9424df851ea6f100
Shaheed Headley <74279457+ObsidioSteel@users.noreply.github.com>
2024-08-26 18:20:20 -0400

    Finished all language quizzes
    The 5-question English, German, French, and Spanish quizzes are all done with a proper dropdown menu and scoring popup when submitting.

--- javascript/quiz.js
+++ javascript/quiz.js
@@ -1,3 +1,4 @@
+//Homepage dropdown menu functions
 function check1(){
     const quiz = document.getElementById("SELECT1");
 
@@ -5,18 +6,89 @@ function check1(){
         quiz.removeChild(quiz.firstElementChild);
     }
 
-    var x = document.getElementById("SELECT1");
+    var choice = document.getElementById("SELECT1").options[document.getElementById("SELECT1").selectedIndex].text;
 
-    if(x.value == "English"){
-        document.getElementById("LangStart").setAttribute("href", "english_quiz.html");
+
+    if(choice == "English"){
+        document.getElementById("LangStart1").setAttribute("href", "english_quiz.html");
     }
-    if(x.value == "French"){
-        document.getElementById("LangStart").setAttribute("href", "french_quiz.html");
+    if(choice == "French"){
+        document.getElementById("LangStart1").setAttribute("href", "french_quiz.html");
     }
-    if(x.value == "German"){
-        document.getElementById("LangStart").setAttribute("href", "german_quiz.html");
+    if(choice == "German"){
+        document.getElementById("LangStart1").setAttribute("href", "german_quiz.html");
     }
-    if(x.value == "Spanish"){
-        document.getElementById("LangStart").setAttribute("href", "spanish_quiz.html");
+    if(choice == "Spanish"){
+        document.getElementById("LangStart1").setAttribute("href", "spanish_quiz.html");
     }
+}
+
+function check2(){
+    const quiz = document.getElementById("SELECT2");
+
+    if(quiz.children[0].value == "Choose2"){
+        quiz.removeChild(quiz.firstElementChild);
+    }
+
+    var choice = document.getElementById("SELECT2").options[document.getElementById("SELECT1").selectedIndex].text;
+
+
+    if(choice == "English"){
+        document.getElementById("LangStart2").setAttribute("href", "english_quiz.html");
+    }
+    if(choice == "French"){
+        document.getElementById("LangStart2").setAttribute("href", "french_quiz.html");
+    }
+    if(choice == "German"){
+        document.getElementById("LangStart2").setAttribute("href", "german_quiz.html");
+    }
+    if(choice == "Spanish"){
+        document.getElementById("LangStart2").setAttribute("href", "spanish_quiz.html");
+    }
+}
+
+function check3(){
+    const quiz = document.getElementById("SELECT3");
+
+    if(quiz.children[0].value == "Choose3"){
+        quiz.removeChild(quiz.firstElementChild);
+    }
+
+    var choice = document.getElementById("SELECT3").options[document.getElementById("SELECT1").selectedIndex].text;
+
+
+    if(choice == "English"){
+        document.getElementById("LangStart3").setAttribute("href", "english_quiz.html");
+    }
+    if(choice == "French"){
+        document.getElementById("LangStart3").setAttribute("href", "french_quiz.html");
+    }
+    if(choice == "German"){
+        document.getElementById("LangStart3").setAttribute("href", "german_quiz.html");
+    }
+    if(choice == "Spanish"){
+        document.getElementById("LangStart3").setAttribute("href", "spanish_quiz.html");
+    }
+}
+
+
+//Language quiz functions
+function submit_quiz() {
+    var score = 0;
+    if(document.getElementById("correct1").checked) {
+        score++;
+    }
+    if(document.getElementById("correct2").checked) {
+        score++;
+    }
+    if(document.getElementById("correct3").checked) {
+        score++;
+    }
+    if(document.getElementById("correct4").checked) {
+        score++;
+    }
+    if(document.getElementById("correct5").checked) {
+        score++;
+    }
+    alert("You got: " + score + "/5! Your score is: " + (score/5)*100 + "%");
 }
